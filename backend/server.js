518991c72a109e07536a756c5968e657367ce18b
Gustavo Souza <gustasouza148@gmail.com>
2026-06-01 10:24:39 -0300

    fix: support render dynamic port

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,8 @@ app.get("/clima/:cidade", async (req, res) => {
     });
   }
 });
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
